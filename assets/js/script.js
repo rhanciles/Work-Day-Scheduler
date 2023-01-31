adb9ebfc29b66806412e4bec42c448042c5c640b
rhanciles <119437254+rhanciles@users.noreply.github.com>
2023-01-31 21:38:01 +0000

    Add borders and style for definition

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,5 @@
 var currentDate = moment().format("dddd Do MMMM YYYY"); 
 $('#currentDay').text(currentDate);
 // var shoppingListEl = $('#shopping-list');
+var currentTime = moment().format("LTS"); 
+$('#currentTime').text(currentTime);
