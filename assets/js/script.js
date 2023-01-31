ede4d03abafb1236d8f0aa1f3761027f8a9d8a40
rhanciles <119437254+rhanciles@users.noreply.github.com>
2023-01-31 21:50:26 +0000

    Add current time display to the header

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,39 @@ $('#currentDay').text(currentDate);
 // var shoppingListEl = $('#shopping-list');
 var currentTime = moment().format("LTS"); 
 $('#currentTime').text(currentTime);
+
+var timeGen = 8
+var timeBlock = $('.time-block');
+var timeRow = $('.time-row');
+var tempRow = $('.temp-row');
+
+function renderSchedule() {
+    // Dynamically create rows
+    // Create a for-loop to iterate through the time slots.
+    for (var i = 0; i < 9; i++) {
+        timeTxt = timeGen[i]
+        timeGen += 1
+    // Use variables to dynamically replicate "newRow".
+    var newRow = $('<ul class="time-row">');
+    var timeCell = $('<li class="time-cell">');
+    var textCell = $('<li class="text-cell">');
+    var saveBtn = $('<li class="fas fa-save save-btn">');
+    
+    // Append new row to exixting block
+    timeBlock.append(newRow)
+    newRow.append(timeCell, textCell, saveBtn);
+
+    // Display generated time in text field
+    timeCell.text(timeGen+(':00'))
+
+    if (timeGen == 9) {
+        timeCell.text(('0')+timeGen+(':00'))
+      }
+
+    console.log(timeRow)
+    console.log(timeGen)
+    
+    }
+}
+$(tempRow).hide();
+renderSchedule()
